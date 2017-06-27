4e479418fe5613b22efa6f467dab8ee7573761ac
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-06-27 17:50:42 +0000

    Gemfile debugging

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout 'portfolio'
   def index
     @portfolio_items = Portfolio.all
@@ -26,11 +27,9 @@ class PortfoliosController < ApplicationController
   end
   
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
   end
   
   def update
-    @portfolio_item = Portfolio.find(params[:id])
       
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
@@ -42,13 +41,9 @@ class PortfoliosController < ApplicationController
   end
   
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end 
   
   def destroy
-    #performs lookup
-     @portfolio_item = Portfolio.find(params[:id])
-     
      #destroy or delete the record
      @portfolio_item.destroy
     respond_to do |format|
@@ -64,5 +59,10 @@ class PortfoliosController < ApplicationController
                                       :body, 
                                       technologies_attributes: [:name]
                                       )
+  end
+
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
   end
 end
